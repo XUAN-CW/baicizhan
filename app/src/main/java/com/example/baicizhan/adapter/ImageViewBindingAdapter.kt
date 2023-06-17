8bfab3ae0b48a00f6c980a5b5591ea23a00b3511
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2023-06-17 16:47:31 +0800

    cd

--- app/src/main/java/com/example/baicizhan/adapter/ImageViewBindingAdapter.kt
+++ app/src/main/java/com/example/baicizhan/adapter/ImageViewBindingAdapter.kt
@@ -3,7 +3,9 @@ package com.example.baicizhan.adapter
 import android.content.Context
 import android.net.Uri
 import android.text.TextUtils
+import android.view.View
 import android.widget.ImageView
+import android.widget.VideoView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import java.io.File
@@ -22,21 +24,49 @@ class ImageViewBindingAdapter(var context: Context) {
                     ).endsWith(".png")
                 ) {
                     imageView.setImageURI(Uri.fromFile(File(url)))
+                    return
                 }
                 if (url.lowercase(Locale.getDefault()).endsWith(".gif")) {
                     Glide.with(imageView.context)
                         .asGif()
                         .load(File(url))
                         .into(imageView)
+                    return
                 }
-                if (url.lowercase(Locale.getDefault()).endsWith(".mp4")) {
-                    Glide
-                        .with(imageView.context)
-                        .load(Uri.fromFile(File(url)))
-                        .into(imageView)
-                }
+                imageView.visibility = View.GONE
+                return
             }
         }
 
+        @JvmStatic
+        @BindingAdapter("videoUrl")
+        fun setImage(videoView: VideoView, url: String) {
+            if (!TextUtils.isEmpty(url)) {
+
+                if(url.lowercase(Locale.getDefault()).endsWith(".mp4")){
+
+                    val video = File(url)
+
+
+                    videoView.setVideoURI(Uri.parse(video.absolutePath))
+
+                    // Remove the sound
+                    videoView.setOnPreparedListener { mediaPlayer ->
+                        mediaPlayer.setVolume(0f, 0f)
+                    }
+
+                    // Remove the media controller bar
+                    videoView.setMediaController(null)
+
+                    // Loop the video
+                    videoView.setOnCompletionListener { videoView.start() }
+
+                    videoView.start()
+                }else{
+                    videoView.visibility = View.GONE
+                }
+
+            }
+        }
     }
 }
